54bf6b4a59985191123302c895a59af343a8256e
Arsy <ariesyarwani21@gmail.com>
2023-03-25 12:17:58 +0800

    admin lte

---
+++ app/Models/Post.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Models;
+
+use Eloquent as Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+/**
+ * Class Post
+ * @package App\Models
+ * @version March 25, 2023, 1:57 am UTC
+ *
+ */
+class Post extends Model
+{
+    use SoftDeletes;
+
+    use HasFactory;
+
+    public $table = 'posts';
+    
+
+    protected $dates = ['deleted_at'];
+
+
+
+    public $fillable = [
+        
+    ];
+
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        
+    ];
+
+    /**
+     * Validation rules
+     *
+     * @var array
+     */
+    public static $rules = [
+        
+    ];
+
+    
+}
